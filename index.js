9514d5249973beda9b42bf52d4300579f967dc5c
Jaime De Lizaola <jaimedelizaola@gmail.com>
2024-11-07 01:46:24 -0600

    Terminada la v1.0

---
+++ index.js
@@ -0,0 +1,10 @@
+const botonenviar = document.getElementById('boton-enviar')
+botonenviar.addEventListener('click', enviar)
+
+const inputUsuario = document.getElementById('busqueda')
+
+console.log(inputUsuario)
+function enviar (){
+    
+    // alert('Aún no contamos con esa información, vuelve mas tarde')
+}
